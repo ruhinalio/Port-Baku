ea11652931436d91234c31bb99c113d1008d3fff
ruhinalio <116165822+ruhinalio@users.noreply.github.com>
2023-04-08 01:28:33 +0400

    Last commit

---
+++ src/components/News-Rezerved/Index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const News = () => {
+  return (
+    <div>Index</div>
+  )
+}
+const Reserved = () => {
+  return (
+    <div>Index</div>
+  )
+}
+
+export default {News, Reserved}
