6af4d412d21a19ec296613a7681700b048700ec3
ruhinalio <116165822+ruhinalio@users.noreply.github.com>
2023-04-10 16:54:01 +0400

    last commit

---
+++ src/pages/Contact/Index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./Style.css"
+
+const Index = () => {
+  return (
+    <div>Index</div>
+  )
+}
+
+export default Index
--- src/pages/Events/Index.jsx
+++ src/pages/Events/Index.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import Xeberler from "../../components/News-Rezerved/MenuEvents";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
 import "./Style.css";
 //components
 import Header from "../../components/Header/Index";
 import Footer from "../../components/Footer/Index";
 import WhiteBtn from "../../components/Button-white/Index";
+
 const Index = () => {
   return (
     <div>
@@ -20,42 +22,72 @@ const Index = () => {
           </p>
         </div>
         <div className="body-events">
-          <div className="actions-events">
-            <NavLink to="/events" className="active">
-              Events
-            </NavLink>
-            <NavLink to="/events/rezerved">Reserved</NavLink>
-          </div>
-          <div className="event-side">
-            <div className="events-card-row">
-              <div className="evend-side-card">
-                <img
-                  src="https://portbakumall.az/resized/fit463x300/center/pages/27/custom-resized-3ca1ee2a-87f5-410a-b1a5-2f5280d0323c.jpg"
-                  alt=""
-                />
-                <h4>National Costume Exhibition at Port Baku</h4>
-                <p>
-                  On the eve of Novruz Bayram, an exhibition featuring
-                  imitations of ancient traditional women's national costumes
-                  from the Karabakh region is being held at the Port Baku Mall.
-                </p>
-                <WhiteBtn text="Read More" />
+         <div className="header-margin"></div>
+          <Tabs>
+            <TabList className="test">
+              <div className="empty-margin"></div>
+              <Tab><h2 className="tab-name">Events</h2></Tab>
+              <Tab><h2 className="tab-name">News</h2></Tab>
+              <div className="empty-margin"></div>
+            </TabList>
+
+            <TabPanel>
+              <div className="event-side">
+                <div className="events-card-row">
+                  <div className="evend-side-card">
+                    <img
+                      src="https://portbakumall.az/resized/fit463x300/center/pages/27/custom-resized-3ca1ee2a-87f5-410a-b1a5-2f5280d0323c.jpg"
+                      alt=""
+                    />
+                    <h4>National Costume Exhibition at Port Baku</h4>
+                    <p>
+                      On the eve of Novruz Bayram, an exhibition featuring
+                      imitations of ancient traditional women's national
+                      costumes from the Karabakh region is being held at the
+                      Port Baku Mall.
+                    </p>
+                    <WhiteBtn text="Read More" />
+                  </div>
+                  <div className="evend-side-card">
+                    <img
+                      src="https://portbakumall.az/resized/fit463x300/center/pages/22/img-2258.jpg"
+                      alt=""
+                    />
+                    <h4>National Costume Exhibition at Port Baku</h4>
+                    <p>
+                      On the eve of Novruz Bayram, an exhibition featuring
+                      imitations of ancient traditional women's national
+                      costumes from the Karabakh region is being held at the
+                      Port Baku Mall.
+                    </p>
+                    <WhiteBtn text="Read More" />
+                  </div>
+                </div>
               </div>
-              <div className="evend-side-card">
-                <img
-                  src="https://portbakumall.az/resized/fit463x300/center/pages/27/custom-resized-3ca1ee2a-87f5-410a-b1a5-2f5280d0323c.jpg"
-                  alt=""
-                />
-                <h4>National Costume Exhibition at Port Baku</h4>
-                <p>
-                  On the eve of Novruz Bayram, an exhibition featuring
-                  imitations of ancient traditional women's national costumes
-                  from the Karabakh region is being held at the Port Baku Mall.
-                </p>
-                <WhiteBtn text="Read More" />
+            </TabPanel>
+            <TabPanel>
+              <div className="news-side">
+                <div className="news-side-row">
+                  <div className="news-side-card">
+                    <img src="https://portbakumall.az/resized/fit463x300/center/pages/194/resized.jpg" alt="" />
+                    <h4>
+                    Feel the Arrival of Spring at Port Baku Shopping Center
+                    </h4>
+                    <p>From March 21-23, Port Baku Mall welcomes its visitors with a festive atmosphere.</p>
+                    <WhiteBtn text="More news"/>
+                  </div>
+                  <div className="news-side-card">
+                    <img src="https://portbakumall.az/resized/fit463x300/center/pages/194/resized.jpg" alt="" />
+                    <h4>
+                    Feel the Arrival of Spring at Port Baku Shopping Center
+                    </h4>
+                    <p>From March 21-23, Port Baku Mall welcomes its visitors with a festive atmosphere.</p>
+                    <WhiteBtn text="More news"/>
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
+            </TabPanel>
+          </Tabs>
         </div>
       </div>
       <div className="container">
